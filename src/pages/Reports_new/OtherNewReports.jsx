32729d2c0cb1bcef40d4a1ab083bea6ca5f244ab
ItsAnshujain <anshujain8844@gmail.com>
2024-11-06 17:27:02 +0530

    updated

--- src/pages/Reports_new/OtherNewReports.jsx
+++ src/pages/Reports_new/OtherNewReports.jsx
@@ -3,7 +3,7 @@ import { Button } from '@mantine/core';
 import PerformanceCard from '../../components/modules/newReports/performanceCard';
 import RevenueCards from '../../components/modules/newReports/RevenueCards';
 import { downloadExcel } from '../../apis/requests/report.requests';
-import { useDownloadExcel, useShareReport } from '../../apis/queries/report.queries';
+import { useDownloadExcel } from '../../apis/queries/report.queries';
 import { showNotification } from '@mantine/notifications';
 import TagwiseReport from '../../components/modules/newReports/TagwiseReport';
 import SalesDistribution from '../../components/modules/newReports/SalesDistribution';
@@ -21,30 +21,33 @@ import InvoiceAmountCollReport from '../../components/modules/newReports/Invoice
 import RevenueBreakup from '../../components/modules/newReports/RevenueBreakup';
 import CampaignCards from '../../components/modules/newReports/CampaingCards';
 import SalesOverview from '../../components/modules/newReports/SalesOverview';
+import html2pdf from 'html2pdf.js';
+import { useState } from 'react';
 import classNames from 'classnames';
-import { downloadPdf } from '../../utils';
 
 const OtherNewReports = () => {
-   // For PDF Download
-   const { mutateAsync: mutateAsyncPdf, isLoading: isDownloadPdfLoading } = useShareReport();
+  //For Pdf Download
+  const [isDownloadPdfLoading, setIsDownloadPdfLoading] = useState(false);
 
-   const handleDownloadPdf = async () => {
-     const activeUrl = new URL(window.location.href);
-     await mutateAsyncPdf(
-       { url: activeUrl.toString() },
-       {
-         onSuccess: data => {
-           showNotification({
-             title: 'Report has been downloaded successfully',
-             color: 'green',
-           });
-           if (data?.link) {
-             downloadPdf(data.link);
-           }
-         },
-       },
-     );
-   };
+  const handleDownloadPdf = () => {
+    setIsDownloadPdfLoading(true);
+
+    const url = new URL(window.location);
+    url.searchParams.set('share', 'report');
+    window.history.pushState({}, '', url);
+
+    const element = document.getElementById('New_reports');
+
+    html2pdf()
+      .set({ filename: 'report.pdf', html2canvas: { scale: 2 } })
+      .from(element)
+      .save()
+      .finally(() => {
+        setIsDownloadPdfLoading(false);
+        url.searchParams.delete('share');
+        window.history.pushState({}, '', url);
+      });
+  };
 
   // For Excel Download
   const { mutateAsync: mutateAsyncExcel, isLoading: isDownloadExcelLoading } = useDownloadExcel();
@@ -73,43 +76,34 @@ const OtherNewReports = () => {
       },
     );
   };
-  
   const isReport = new URLSearchParams(window.location.search).get('share') === 'report';
 
   return (
-    <div
-      className={classNames(
-        'overflow-y-auto p-3 overflow-hidden',
-        !isReport ? 'col-span-10 ' : 'col-span-12',
-      )}
-    >
-      {!isReport ? (
-        <div className="flex flex-end justify-end ">
-          <div className="py-5 flex items-start">
-            <Button
-              leftIcon={<Download size="20" color="white" />}
-              className="primary-button mx-3 pdf_download_button"
-              onClick={handleDownloadPdf}
-              loading={isDownloadPdfLoading}
-              disabled={isDownloadPdfLoading}
-            >
-              Download PDF
-            </Button>
-          </div>
-          <div className="py-5 flex items-start">
-            <Button
-              leftIcon={<Download size="20" color="white" />}
-              className="primary-button"
-              onClick={handleDownloadExcel}
-              loading={isDownloadExcelLoading}
-              disabled={isDownloadExcelLoading}
-            >
-              Income Statement
-            </Button>
-          </div>
+    <div className="overflow-y-auto p-3 col-span-10 overflow-hidden">
+      <div className="flex flex-end justify-end ">
+        <div className="py-5 flex items-start">
+          <Button
+            leftIcon={<Download size="20" color="white" />}
+            className="primary-button mx-3 pdf_download_button"
+            onClick={handleDownloadPdf}
+            loading={isDownloadPdfLoading}
+            disabled={isDownloadPdfLoading}
+          >
+            Download PDF
+          </Button>
         </div>
-      ) : null}
-
+        <div className="py-5 flex items-start">
+          <Button
+            leftIcon={<Download size="20" color="white" />}
+            className="primary-button"
+            onClick={handleDownloadExcel}
+            loading={isDownloadExcelLoading}
+            disabled={isDownloadExcelLoading}
+          >
+            Income Statement
+          </Button>
+        </div>
+      </div>
       <div id="New_reports">
         <div className="border-2 p-5 border-black">
           <p className="font-bold text-lg"> Revenue </p>
@@ -127,11 +121,11 @@ const OtherNewReports = () => {
           <SalesDistribution />
           <SalesComparision />
         </div>
-        <div className="border-2 p-5 border-black my-2">
+        <div className='border-2 p-5 border-black my-2'>
           <OperationalCosts />
           <PrintingMountingCosts />
         </div>
-        <div className="border-2 p-5 border-black my-2">
+        <div className='border-2 p-5 border-black my-2'>
           <p className="font-bold text-lg"> Client Data</p>
           <CampaignCards />
           <CampaignDetails />
